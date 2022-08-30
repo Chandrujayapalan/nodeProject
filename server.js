8721c42b3573b944936265011ba28652c6af6bd8
Chandrujayapalan <108114432+Chandrujayapalan@users.noreply.github.com>
2022-08-30 12:11:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyparser.json())
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`server is running ${PORT}`)
-    res.json({ message : "api is running"})
+ 
 })
 
 app.use('/api', userController)
